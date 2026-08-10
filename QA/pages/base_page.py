daed7400d8893d55a1eae23fa423bccbd97b7d1c
was005032606-collab <was005032606@gmail.com>
2026-08-10 15:01:41 +0200

    Create base_page.py

---
+++ QA/pages/base_page.py
@@ -0,0 +1,13 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def find(self, locator):
+        return self.wait.until(EC.presence_of_element_located(locator))
+
+    def click(self, locator):
+        self.wait.until(EC.element_to_b
